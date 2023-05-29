e8905c41188be3863182753ec22620a4761a2919
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-05-29 19:45:14 +0300

    Update 06-numberInRange.js

--- 3.0 Conditional Statements Advanced - Lab/06-numberInRange.js
+++ 3.0 Conditional Statements Advanced - Lab/06-numberInRange.js
@@ -3,7 +3,7 @@ n => +n >= -100 && +n <= 100 && +n !== 0 ? 'Yes' : 'No'
 //---------------------------------(2)--------------------------
 
 function numberInRange(n) {
-  return +n >= -100 && +n <= 100 && +n !== 0 ? 'Yes' : 'No'
+  return +n >= -100 && +n <= 100 && +n != 0 ? 'Yes' : 'No'
 }
 
 // console.log(numberInRange([0]))
