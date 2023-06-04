a42228ccd727cfe1c25243c3a932c57c52caa346
byAbaddon <51271834+byAbaddon@users.noreply.github.com>
2023-06-04 14:02:16 +0300

    Update 08. Basketball Equipment.js

--- 1.1 First Steps In Coding - Exercise/08. Basketball Equipment.js
+++ 1.1 First Steps In Coding - Exercise/08. Basketball Equipment.js
@@ -6,11 +6,11 @@ t => {
 //---------------------------------(2)--------------------------
 
 function basketballEquipment(tax) {
-  let sneakers = +tax * 0.60
-  let outfit = sneakers * 0.80
+  let sneakers = tax * 0.6
+  let outfit = sneakers * 0.8
   let ball = outfit / 4
   let accessories = ball / 5
-  return `${(+tax + sneakers + outfit + ball + accessories).toFixed(2)}`
+  return +tax + sneakers + outfit + ball + accessories
 }
 
 //console.log(basketballEquipment(320))
